45a5abac52654122c8ef3094973fbfdab5f83931
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2023-12-29 21:55:02 +0530

    Updating leap year program

--- leap_year.py
+++ leap_year.py
@@ -19,11 +19,11 @@ year=int(input("Enter a year:"))
 # else:
 #     print(year," is not leap year")
 
-# if year%4==0 and year%100!=0:
-#     print("Leap year")
-# elif year%400==0 and year%100==0:
-#     print("Leap year")
-# else:
-#     print("Not a leap year")
+if year%4==0 and year%100!=0:
+    print("Leap year")
+elif year%400==0 and year%100==0:
+    print("Leap year")
+else:
+    print("Not a leap year")
 
 
