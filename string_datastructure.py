1c571b3a6d7d02bab3b593f214a91891ceef22ce
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2023-12-31 23:03:54 +0530

    Demo Commit

---
+++ string_datastructure.py
@@ -0,0 +1,16 @@
+str= "Shashi"
+print(str)
+
+#length of string 
+print("Length of str:",len(str))
+
+multiline_string = '''I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python.'''
+print(multiline_string)
+
+# Another way of doing the same thing
+multiline_string = """I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python."""
+print(multiline_string)
