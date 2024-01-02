c3329b4c8e888f0e5da0bf421ed14d9b923b2879
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-01-02 22:01:06 +0530

    30 days of python

---
+++ 30 Days of Python/Day_1_Introduction.py
@@ -0,0 +1,20 @@
+#Introduction
+
+print(type(10))
+print(type(3.14))
+print(type(1+2j))
+print(type("Shashikant"))
+print(type([1,2,3]))
+print(type({1,2,3,}))
+print(type((1,2,3,"name")))
+print(type({"name":"Shashikant"}))
+
+a=10
+b=12
+print("sum:",a+b)
+print("diff:",a-b)
+print("product:",a*b)
+print("quotient:",a/b)
+print("exponant:",a**b)
+print("ramainder",a%b)
+print("true division:",a//b)
---
+++ 30 Days of Python/Day_2_Variables.py
@@ -0,0 +1,129 @@
+#Day 2:30 Days of python programming
+
+#first_name="Shashikant"
+
+# last_name="Mane"
+
+# full_name="Shashikant Mane"
+
+# country="India"
+
+# city="Mumbai"
+
+# age=20
+
+# year=2024
+
+# is_married=False
+
+# is_true=True
+
+# is_light_on=True
+
+# skills=("HTML","CSS")
+
+# Check the data type of all your variables using type() built-in function
+
+# print(type(first_name))
+# print(type(last_name))
+# print(type(full_name))
+# print(type(country))
+# print(type(city))
+# print(type(age))
+# print(type(year))
+# print(type(is_married))
+# print(type(is_true))
+# print(type(is_light_on))
+# print(type(skills))
+
+# Using the len() built-in function, find the length of your first name
+# print(len(first_name))
+# print(len(last_name))
+# print(len(full_name))
+# print(len(country))
+# print(len(city))
+# print(len(str(age)))
+# print(len(str(year)))
+# print(len(str(is_married)))
+# print(len(str(is_true)))
+# print(len(str(is_light_on)))
+# print(len(skills))
+
+# Compare the length of your first name and your last name
+
+# compare=len(first_name)==len(last_name)
+# print(compare)
+
+# Declare 5 as num_one and 4 as num_two
+# Add num_one and num_two and assign the value to a variable total
+# Subtract num_two from num_one and assign the value to a variable diff
+# Multiply num_two and num_one and assign the value to a variable product
+# Divide num_one by num_two and assign the value to a variable division
+# Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+# Calculate num_one to the power of num_two and assign the value to a variable exp
+# Find floor division of num_one by num_two and assign the value to a variable floor_division
+
+num_one=5
+num_two=4
+
+variable_total=num_one+num_two
+print(variable_total)
+
+variable_diff=num_one-num_two
+print(variable_diff)
+
+variale_product=num_one*num_two
+print(variale_product)
+
+variable_division=num_one/num_two
+print(variable_division)
+
+variable_remainder=num_one%num_two
+print(variable_remainder)
+
+variable_exp=num_one**num_two
+print(variable_exp)
+
+variable_floor_divison=num_one//num_two
+print(variable_floor_divison)
+
+# The radius of a circle is 30 meters.
+# Calculate the area of a circle and assign the value to a variable name of area_of_circle
+# Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+# Take radius as user input and calculate the area.
+
+radius=30
+
+area_of_circle=3.14*radius*radius
+
+print("Area of circle:",area_of_circle)
+# ================================================
+r=float(input("Enter radius:"))
+
+circumference_of_circle=2*3.14*r
+
+print("Circumference of circle:",circumference_of_circle)
+
+# Use the built-in input function to get first name, last name,
+# country and age from a user and store the value to their corresponding variable names
+
+first_name=input("Enter first name:")
+
+last_name=input("Enter last name:")
+
+country=input("Enter country:")
+
+age=int(input("Enter Age:"))
+
+print(first_name)
+print(last_name)
+print(country)
+print(age)
+
+# Run help('keywords') in Python shell or in your file to check 
+# for the Python reserved words or keywords
+
+help('keywords')
+
+
+
