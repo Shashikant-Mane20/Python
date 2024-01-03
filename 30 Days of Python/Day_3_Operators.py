a55eaccc883e5ecf84cab1de82302af998d61049
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-01-03 21:22:46 +0530

    Day_3_Operators

---
+++ 30 Days of Python/Day_3_Operators.py
@@ -0,0 +1,91 @@
+# Declare your age as integer variable
+
+age=20
+
+# Declare your height as a float variable
+
+# height=165.5
+
+# Declare a variable that store a complex number
+
+complex=1+2j
+
+
+# Write a script that prompts the user to enter base and 
+# height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+
+# base=int(input("Enter base:"))
+
+# height=int(input("Enter height:"))
+
+# area=0.5*base*height
+
+# print("Area of triangle:",area)
+
+
+# Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
+# Calculate the perimeter of the triangle (perimeter = a + b + c).
+
+# a=int(input("Enter side a:"))
+# b=int(input("Enter side b:"))
+# c=int(input("Enter side c:"))
+
+# perimeter=a+b+c
+
+# print("Perimeter of triangle is",perimeter)
+
+
+# Get length and width of a rectangle using prompt. 
+# Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+
+#calculate area of rectangle
+# length=int(input("Enter length of reactangle:"))
+
+# width=int(input("Enter width of rectangle:"))
+
+# area=length*width
+
+# print("Area of rectangle is",area)
+
+# #perimeter of rectangle
+# perimeter=2*(length+width)
+
+# print("Perimeter of rectangle is",perimeter)
+
+
+# Get radius of a circle using prompt. Calculate the area (area = pi x r x r) 
+# and circumference (c = 2 x pi x r) where pi = 3.14.
+
+# radius=int(input("Enter radius of circle:"))
+
+#calculate area of circle
+# pi=3.14
+# area=pi*radius*radius
+
+# print("Area of circle is",area)
+
+#circumference of circle
+
+# c=2*pi*radius
+
+# print("Circumference of circle is",c)
+
+# Calculate the slope, x-intercept and y-intercept of y = 2x -2
+
+#Given equation is y= 2x-2 => y= mx+b
+
+m=2
+b=-2
+
+x_intercept=-b/m
+
+y_intercept=2*(0)-2
+
+print("Slope is:",m)
+print("x_intercept:",x_intercept)
+print("y_intercept:",y_intercept)
+
+p=len("python")
+d=len("dragon")
+
+print(p==d)
