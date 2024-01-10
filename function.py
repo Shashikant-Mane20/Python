d85c299373bb76c74060313e94d443841ff37f7a
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-01-10 22:35:04 +0530

    Funtion in Python

---
+++ function.py
@@ -0,0 +1,15 @@
+# def addition(x,y):
+#     res=x+y
+#     print(res)
+# addition(10,20)
+
+#using return
+
+def addition(n1,n2):
+    n3=n1+n2
+    return n3
+
+n1=12
+n2=13
+res=addition(n1,n2)
+print(res)
