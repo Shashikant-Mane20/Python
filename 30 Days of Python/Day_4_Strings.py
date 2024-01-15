f00b336364cd610539f3cc954e834bb159c58151
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-01-15 23:07:49 +0530

    30 Days of Python

--- 30 Days of Python/Day_4_Strings.py
+++ 30 Days of Python/Day_4_Strings.py
@@ -2,5 +2,18 @@ first_name="Shashikant"
 
 last_name="Mane"
 
+#string concatenation operator
 full_name=first_name+last_name
 print(full_name)
+
+#string repetition operator
+rep=3*first_name
+print(rep)
+
+#length of string
+l=len(full_name)
+print(l)
+
+#Accessing element from string
+s='I am Shashikant'
+print(s[0:8:2])
