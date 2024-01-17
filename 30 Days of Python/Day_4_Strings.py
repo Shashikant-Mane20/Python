a81f4beee0e58d03d4c0b8ef2d1695b4a857e7ce
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-01-17 21:00:24 +0530

    30 Days of Python

--- 30 Days of Python/Day_4_Strings.py
+++ 30 Days of Python/Day_4_Strings.py
@@ -18,4 +18,36 @@ print(l)
 s='I am Shashikant'
 print(s[0:8:2])
 print(s[:-6:-1])
-print(s[-1:6])
+print(s[-1:-6:-1])
+print(s[-1:-5:-1])
+print(s[5])
+print(s[-4])
+print(s[-10::1])
+
+#Slicing with the third parameter
+
+s='abc'*3
+print(s)
+print(s[::3]) #start 0,end length,step 1
+
+#slicin with the third parameter
+print(s[1::3])
+print(s[:5:3])
+
+#negative Third Prameter
+# default steps -1
+
+n="I am Shashikant"
+
+print(n[-1:-5:-1])
+print(n[14:10:-1]) #direction is rigth to left
+
+#Reverse a string
+print(n[::-1])
+
+str='Shashikant'
+
+for x in str:
+    print(x)
+
+
---
+++ 30 Days of Python/Day_6_lists.py
@@ -0,0 +1,38 @@
+# lists
+# a collection similar or different types of data itemes
+
+l=[1,2,3,4,5,6,7,8,9,10]
+# start 0 and negative is -1
+#length
+print(len(l))
+
+print(l)
+
+#creating a list
+
+# [] seprated by commas
+
+e=[]
+print(e) #empty list
+
+#a list with duplicate items
+
+num=[1,2,2,3,4,4,5,6,6]
+print(num)
+
+#creating list with different types of dataitmes
+
+items=[1,2,'N','Go',3.14]
+
+#features
+
+# A list can be duplicate items
+# items in alist are mutable
+# lists can store items of various data types
+
+
+# Accessing elements of a single dimensional list
+# list where elements are listed one after the other
+#each element is allocated a unique number called index
+
+print(items[2])
