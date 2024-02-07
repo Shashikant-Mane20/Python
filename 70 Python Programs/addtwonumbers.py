bed0eb59b4ebe7957bb383dcb6a59b4a2092e139
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-02-07 23:09:54 +0530

    Python Programs

---
+++ 70 Python Programs/addtwonumbers.py
@@ -0,0 +1,8 @@
+#Addition of two numbers
+num1=int(input("Enter number1:"))
+
+num2=int(input("Enter number2:"))
+
+add=num1 + num2
+
+print("Addition of "+str(num1)+" and "+str(num2)+" is :",add)
---
+++ 70 Python Programs/squareroot.py
@@ -0,0 +1,6 @@
+import math
+num = int(input("Enter any number:"))
+
+squareroot=math.sqrt(num)
+
+print("Sqaure root of"+str(num)+" is :",squareroot)
