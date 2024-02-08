f8f9392c09d2236a231c8a709f0e6b3555b66556
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-02-09 00:34:03 +0530

    Python Programs

--- 70 Python Programs/helloworld.py
+++ 70 Python Programs/helloworld.py
@@ -1,3 +1,30 @@
-hello="Hello world"
 
-print(hello)
+# total = 0
+# current_number = 100
+
+# while current_number <= 200:
+   
+#     if current_number % 2 == 0:
+    
+#         total += current_number
+
+#     current_number += 1
+# print("Sum of even numbers between 100 and 200:", total)
+
+
+# total = 0
+# c_n=100
+
+# while c_n <= 200:
+#     if c_n % 2 == 0:
+#         total += c_n
+
+#     c_n +=1
+# print("Sum of even numbers between 100 and 200:",total)
+
+n=5
+for i in range(1,n+1,1):
+    
+    for j in range(i):
+        print(i,end="")
+    print()
--- 70 Python Programs/squareroot.py
+++ 70 Python Programs/squareroot.py
@@ -1,6 +1,31 @@
-import math
-num = int(input("Enter any number:"))
+# import math
+# num = int(input("Enter any number:"))
 
-squareroot=math.sqrt(num)
+# squareroot=math.sqrt(num)
 
-print("Sqaure root of"+str(num)+" is :",squareroot)
+# print("Sqaure root of"+str(num)+" is :",squareroot)
+
+
+ui= int(input("Enter 4 digit number:"))
+
+a= ui%10#4
+
+b= ui//10
+
+c=b%10#3
+
+d=b//10
+
+e=d%10#2
+
+f=d//10#1
+
+j=(a**4+c**4+e**4+f**4)
+print(j)
+
+if j == ui:
+    print("It is a armstrong number")
+else:
+    print("It is not a armstrong number")
+
+
